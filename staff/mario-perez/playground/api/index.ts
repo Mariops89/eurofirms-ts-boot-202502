d94bb3ecc2d159666c0d52dee0aee6c09a607a32
Mariops89 <mariops89@hotmail.com>
2025-09-19 01:56:05 +0200

    split api into routes with express router; add error handling middlewaare; #5

--- staff/mario-perez/playground/api/index.ts
+++ staff/mario-perez/playground/api/index.ts
@@ -1,11 +1,8 @@
 import mongoose from "mongoose"
 import express from "express"
 import cors from "cors"
-import jwt from "jsonwebtoken"
-import { errors } from "com"
-import { logic } from "./logic"
-
-const { SystemError } = errors
+import { usersRouter } from "./routes"
+import { errorHandler } from "./middlewares"
 
 const { JWT_SECRET, PORT, MONGO_URL } = process.env
 
@@ -24,58 +21,14 @@ mongoose.connect(MONGO_URL!)
 
         api.use(express.json())
 
-        api.post('/users', (req, res) => {
-            try {
-                const { name, email, username, password } = req.body
+        api.use(errorHandler)
 
-                logic.registerUser(name, email, username, password)
-                    .then(() => res.status(201).send())
-                    .catch(error => res.status(500).json({ error: SystemError.name, message: error.message }))
-            } catch (error) {
-                res.status(500).json({ error: SystemError.name, message: (error as Error).message })
-            }
 
-        })
 
-        api.post('/users/auth', (req, res) => {
-            try {
-                const { name, email, username, password } = req.body
+        const usersRouter = express.Router()
 
-                logic.authenticateUser(username, password)
-                    .then(userId => {
-                        const token = jwt.sign({ sub: userId }, JWT_SECRET!)
 
-                        res.json(token)
-                    })
-                    .catch(error => res.status(500).json({ error: SystemError.name, message: error.message }))
-            } catch (error) {
-                res.status(500).json({ error: SystemError.name, message: (error as Error).message })
-            }
-
-        })
-
-        api.get('/users/self', (req, res) => {
-            try {
-                const authorization = req.headers.authorization
-
-                if (!authorization) {
-                    res.status(400).json({ error: SystemError.name, message: 'no authorization received' })
-
-                    return
-                }
-
-                const token = authorization.slice(7)
-
-                const { sub: userId } = jwt.verify(token, JWT_SECRET!)
-
-                logic.getUser(userId as string)
-                    .then(user => res.json(user))
-                    .catch(error => res.status(500).json({ error: SystemError.name, message: error.message }))
-            } catch (error) {
-                res.status(500).json({ error: SystemError.name, message: (error as Error).message })
-            }
-
-        })
+        api.use('/users', usersRouter)
 
         api.listen(PORT, () => console.log('API is up'))
     })
---
+++ staff/mario-perez/playground/api/routes/index.ts
@@ -0,0 +1,3 @@
+import { usersRouter } from "./usersRouters"
+
+export { usersRouter }
